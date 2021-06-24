4dbfe6f99ba427940072b04fba7bd8ebb65061cb
Piotr Rogowski <rogowski.piotr99@gmail.com>
2021-06-24 19:10:41 +0200

    add reload script

---
+++ test-env/reload.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+APP_FILE=$1
+PID_FILE="${APP_FILE%.*}.pid"
+LOG_FILE="${APP_FILE%.*}.log"
+
+if [ -f $PID_FILE ]; then
+        cat $PID_FILE | xargs kill -9
+fi
+
+nohup java -jar $APP_FILE > $LOG_FILE &
+rm -f $PID_FILE
+pidof java > $PID_FILE
