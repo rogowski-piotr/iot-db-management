61111e19a15d0e832dde8c0581308c6fc26dadc7
Piotr Rogowski <rogowski.piotr99@gmail.com>
2021-08-08 17:15:24 +0200

    Add user and role model

---
+++ model/src/main/java/pl/piotr/iotdbmanagement/role/Role.java
@@ -0,0 +1,33 @@
+package pl.piotr.iotdbmanagement.role;
+
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import pl.piotr.iotdbmanagement.user.User;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@ToString
+@EqualsAndHashCode
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @SequenceGenerator(name = "roles_id_generator", sequenceName = "roles_id_sequence", initialValue = 1, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "roles_id_generator")
+    @Column(name = "id", updatable = false, nullable = false)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ToString.Exclude
+    @OneToMany(mappedBy = "role", fetch = FetchType.LAZY)
+    private List<User> users;
+}
---
+++ model/src/main/java/pl/piotr/iotdbmanagement/role/RoleRepository.java
@@ -0,0 +1,6 @@
+package pl.piotr.iotdbmanagement.role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
