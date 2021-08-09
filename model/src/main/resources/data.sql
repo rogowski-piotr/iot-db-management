2593d478f99ce96d8621e3320a6e88b14d6b7398
Piotr Rogowski <rogowski.piotr99@gmail.com>
2021-08-09 15:53:21 +0200

    Add encoded data

--- model/src/main/resources/data.sql
+++ model/src/main/resources/data.sql
@@ -8,10 +8,10 @@ VALUES ((SELECT nextval ('roles_id_sequence')), 'USER');
 
 -- USERS
 INSERT INTO users (id, name, surname, email, password, role_id)
-VALUES ((SELECT nextval ('users_id_sequence')), 'admin', '', 'admin@example.com', 'admin', 1);
+VALUES ((SELECT nextval ('users_id_sequence')), 'admin', '', 'admin@example.com', '21232f297a57a5a743894a0e4a801fc3', 1); --password: admin
 
 INSERT INTO users (id, name, surname, email, password, role_id)
-VALUES ((SELECT nextval ('users_id_sequence')), 'Jan', 'Kowalski', 'jan@example.com', 'secret', 2);
+VALUES ((SELECT nextval ('users_id_sequence')), 'Jan', 'Kowalski', 'jan@example.com', '5ebe2294ecd0e0f08eab7690d2a6ee69', 2); -- password: secret
 
 
 -- MEASUREMENTS TYPES
