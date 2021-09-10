04ec141936d8388e01a6378fe70b098f21b6f681
Piotr Rogowski <rogowski.piotr99@gmail.com>
2021-09-10 21:06:31 +0200

    reverse sorting

--- client-api/src/main/java/pl/piotr/iotdbmanagement/service/MeasurementService.java
+++ client-api/src/main/java/pl/piotr/iotdbmanagement/service/MeasurementService.java
@@ -82,7 +82,7 @@ public class MeasurementService {
 
         resultList.sort((o1, o2) -> {
             if (o1.getDate() != null && o2.getDate() != null) {
-                return o1.getDate().compareTo(o2.getDate());
+                return o2.getDate().compareTo(o1.getDate());
             } else {
                 return 1;
             }
