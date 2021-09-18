e76a664dced6a4006e0813c1fd10ec30df5fd500
Piotr Rogowski <rogowski.piotr99@gmail.com>
2021-09-18 21:00:57 +0200

    add statistic controller

--- client-api/src/main/java/pl/piotr/iotdbmanagement/controller/StatsController.java
+++ client-api/src/main/java/pl/piotr/iotdbmanagement/controller/StatsController.java
@@ -1,2 +1,32 @@
-package pl.piotr.iotdbmanagement.controller;public class StatsController {
+package pl.piotr.iotdbmanagement.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.piotr.iotdbmanagement.dto.sensor.GetSensorResponse;
+import pl.piotr.iotdbmanagement.dto.sensor.GetSensorsResponse;
+import pl.piotr.iotdbmanagement.dto.stats.GetAllStatsResponse;
+import pl.piotr.iotdbmanagement.service.StatsService;
+import java.util.logging.Logger;
+
+@RestController
+@RequestMapping("api_auth/stats")
+public class StatsController {
+    private Logger logger = Logger.getLogger(this.getClass().getName());
+    private StatsService statsService;
+
+    @Autowired
+    public StatsController(StatsService statsService) {
+        this.statsService = statsService;
+    }
+
+    @GetMapping
+    public ResponseEntity<GetAllStatsResponse> getAllStats() {
+        logger.info("GET all stats");
+        int successConnections = statsService.getAllSuccessStats();
+        int successFailures = statsService.getAllFailuresStats();
+        return ResponseEntity.ok(GetAllStatsResponse.entityToDtoMapper().apply(successConnections, successFailures));
+    }
 }
--- client-api/src/main/java/pl/piotr/iotdbmanagement/dto/stats/GetAllStatsResponse.java
+++ client-api/src/main/java/pl/piotr/iotdbmanagement/dto/stats/GetAllStatsResponse.java
@@ -1,2 +1,26 @@
-package pl.piotr.iotdbmanagement.dto.stats;public class GetAllStatsResponse {
+package pl.piotr.iotdbmanagement.dto.stats;
+
+import lombok.*;
+import java.util.function.BiFunction;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@ToString
+@EqualsAndHashCode
+public class GetAllStatsResponse {
+
+    private Integer successes;
+
+    private Integer failures;
+
+    public static BiFunction<Integer, Integer, GetAllStatsResponse> entityToDtoMapper() {
+        return (success, failures) -> GetAllStatsResponse.builder()
+                .successes(success)
+                .failures(failures)
+                .build();
+    }
+
 }
